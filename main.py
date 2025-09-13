0139c1ac3ad7561b6b126d40f68002ab245dc74b
KanchiMoe <github@chiyo-chan.me>
2025-09-13 14:14:34 +0100

    add code to make bot work as persistant or one-and-done

--- main.py
+++ main.py
@@ -1,24 +1,36 @@
+import asyncio
 from dotenv import load_dotenv
 import logging
 import os
 
-from src.discord.bot import BOT
+import time
 
-DEFAULT_LOG_LEVEL = os.environ.get("LOG_LEVEL") or logging.DEBUG
+from src.discord.bot import BOT, ready_event
+
+DEFAULT_LOG_LEVEL = os.environ.get("LOG_LEVEL") or logging.INFO
 logging.getLogger().setLevel(DEFAULT_LOG_LEVEL)
 logging.basicConfig(format="%(asctime)s - %(levelname)s - %(message)s")
 
-def main():
+async def main():
     DISCORD_BOT_TOKEN = os.getenv('DISCORD_BOT_TOKEN')
     if not DISCORD_BOT_TOKEN:
-        raise RuntimeError("DISCORD_BOT_TOKEN is not set or empty.")
+        err_msg = "DISCORD_BOT_TOKEN is not set or empty."
+        logging.critical(err_msg)
+        raise RuntimeError(err_msg)
+    
+    async with BOT:
+        bot_task = asyncio.create_task(BOT.start(DISCORD_BOT_TOKEN))
+        await BOT.wait_until_ready()
+        # WAIT until on_ready() in bot.py signals fully ready
+        await ready_event.wait()
 
-    BOT.run(DISCORD_BOT_TOKEN)
+        # Other code can go here.
 
+        await bot_task
 
 if __name__ == "__main__":
     if not os.environ.get("NOT_DOTENV"):
         load_dotenv()
         logging.info("dotenv loaded.")
 
-    main()
+    asyncio.run(main())
