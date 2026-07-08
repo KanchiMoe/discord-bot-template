0c9f2bfd8ee37b61e69b453e1f8675330aef615e
KanchiMoe <github@chiyo-chan.me>
2026-07-08 19:40:53 +0100

    remove for better management

--- src/discord/misc_commands.py
+++
@@ -1,8 +0,0 @@
-import discord
-from discord import app_commands
-#from src.bot import BOT
-
-#/ping, all servers
-@app_commands.command(name="ping", description="Ping")
-async def ping_slash(interaction: discord.Interaction):
-    await interaction.response.send_message(":stopwatch: | pong!")
